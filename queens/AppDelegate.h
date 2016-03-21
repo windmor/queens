907f50330da07c9fe56da639cd7f96425bb52dca
windmor <windmor@yandex.ru>
2016-03-21 14:34:14 +0500

    Initial Commit

---
+++ queens/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  queens
+//
+//  Created by Liliya Sayfutdinova on 3/21/16.
+//  Copyright © 2016 Liliya Sayfutdinova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ queens/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  queens
+//
+//  Created by Liliya Sayfutdinova on 3/21/16.
+//  Copyright © 2016 Liliya Sayfutdinova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
